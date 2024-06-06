43629a0ab5033683dce7687447e11c006e3de576
azoozin <mario.github@protonmail.com>
2024-06-06 18:12:18 -0300

    add ChipFrame class

---
+++ src/emulator/ChipFrame.java
@@ -0,0 +1,26 @@
+package emulator;
+
+import chip.Chip;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChipFrame extends JFrame {
+
+    private ChipPanel panel;
+
+    public ChipFrame(Chip chip) {
+        setPreferredSize(new Dimension(640, 320));
+        pack();
+        setPreferredSize(new Dimension(640 + getInsets().left + + getInsets().right,
+                320 + getInsets().top + getInsets().bottom));
+
+        panel = new ChipPanel(chip);
+        setLayout(new BorderLayout());
+        add(panel, BorderLayout.CENTER);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Chip8 Emulator");
+        pack();
+        setVisible(true);
+    }
+}
