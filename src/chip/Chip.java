292a9029e0086a69feb7aaf0493c9295cede1489
azoozin <mario.github@protonmail.com>
2024-06-07 19:55:19 -0300

    stuff

--- src/chip/Chip.java
+++ src/chip/Chip.java
@@ -1,5 +1,10 @@
 package chip;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Chip {
 
     private char[] memory;
@@ -21,6 +26,8 @@ public class Chip {
 
     private byte[] display;
 
+    private boolean needRedraw;
+
     // initiate
     public void init() {
         // kB = 1024, kB * 4 = 4096 == 4kB
@@ -41,36 +48,107 @@ public class Chip {
         // display resolution 64x32
         // scale up 10x to 640x320
         display = new byte[64 * 32];
+
+        needRedraw = false;
     }
-
     public void run() {
-        // fetching operation code
-        // << 8 shifts an entire byte to the left
-        char opcode = (char) ((char)(memory[programCounter] << 8) | memory[programCounter + 1]);
-        System.out.println(Integer.toHexString(opcode));
-
-        // decoding opcodes
-        // get value o first bitwise nibble of opcode
+        //fetches opcode
+        char opcode = (char)((memory[programCounter] << 8) | memory[programCounter + 1]);
+        System.out.print(Integer.toHexString(opcode) + ": ");
+        //decodes the operation code
         switch(opcode & 0xF000) {
 
-            case 0x8000:
-                // ge value last nibble
+            case 0x1000: //1NNN: Jumps to address NNN
+                break;
+
+            case 0x2000: //2NNN: Calls subroutine at NNN
+                stack[stackPointer] = programCounter;
+                stackPointer++;
+                programCounter = (char)(opcode & 0x0FFF);
+                break;
+
+            case 0x3000: //3XNN: Skips the next instruction if VX equals NN
+                break;
+
+            case 0x6000: //6XNN: Set VX to NN
+                int x = (opcode & 0x0F00) >> 8;
+                V[x] = (char)(opcode & 0x00FF);
+                programCounter += 2;
+                break;
+
+            case 0x7000: //7XNN: Adds NN to VX
+                int _x = (opcode & 0x0F00) >> 8;
+                int nn = (opcode & 0x00FF);
+                V[_x] = (char)((V[_x] + nn) & 0xFF);
+                break;
+
+            case 0x8000: //Contains more data in last nibble
+
                 switch(opcode & 0x000F) {
-                    case 0x000:
+
+                    case 0x0000: //8XY0: Sets VX to the value of VY.
                     default:
-                        System.err.println("Operation Code not supported.");
+                        System.err.println("Unsupported Opcode!");
                         System.exit(0);
                         break;
                 }
+
                 break;
+
+            case 0xA000: //ANNN: Set I to NNN
+                I = (char)(opcode & 0x0FFF);
+                programCounter += 2;
+                break;
+
+            case 0xD000: //DXYN: Draw a sprite (X, Y) size (8, N). Sprite is located at I
+                programCounter += 2;
+                break;
+
             default:
-                System.err.println("Operation Code not supported.");
+                System.err.println("Unsupported Opcode!");
                 System.exit(0);
         }
-    //        execute opcode (during/inside decoding func?)
     }
 
     public byte[] getDisplay() {
-        return this.display;
+        return display;
+    }
+
+    public boolean needsRedraw() {
+        return needRedraw;
+    }
+
+    public void removeDrawFlag() {
+        needRedraw = false;
+    }
+
+    public void  loadProgram(String file) {
+        DataInputStream input = null;
+        try {
+             input = new DataInputStream(new FileInputStream(new File(file)));
+
+            int offset = 0;
+            while (input.available() > 0) {
+                memory[0x200 + offset] = (char)(input.readByte() & 0xFF);
+                offset++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
+    }
+
+    public void loadFontset() {
+        for(int i = 0; i < ChipData.fontset.length; i++) {
+            memory[0x50 + i] = (char)(ChipData.fontset[i] & 0xFF);
+        }
     }
 }
--- src/emulator/Main.java
+++ src/emulator/Main.java
@@ -2,7 +2,7 @@ package emulator;
 
 import chip.Chip;
 
-public class Main {
+public class Main extends Thread{
 
     private Chip chip8;
     private ChipFrame frame;
@@ -10,9 +10,29 @@ public class Main {
     public Main() {
         this.chip8 = new Chip();
         chip8.init();
+        chip8.loadProgram("pong2.c8");
         this.frame = new ChipFrame(chip8);
     }
 
+    // basic loop of thread
+    // chip-8 runs at 60 fps/60 hZ
+    public void run() {
+        while (true) {
+            chip8.run();
+            if (chip8.needsRedraw()) {
+                frame.repaint();
+                chip8.removeDrawFlag();
+            }
+            try {
+                Thread.sleep(16);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        Main main = new Main();
+        main.start();
     }
 }
